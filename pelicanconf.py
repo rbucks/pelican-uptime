da17d378d0e0eec908cef0bc3c6bd0d83c5fe7f9
Taras Matsyk <x@tar7k.com>
2022-07-14 10:24:48 +0300

    Decrease build time by 3sec

--- pelicanconf.py
+++ pelicanconf.py
@@ -10,8 +10,14 @@ DEFAULT_LANG = 'en'
 
 THEME = "themes/uptime"
 
-STATIC_PATHS = ['images', 'extra/robots.txt', 'extra/favicon.ico',
-                'extra/sitemap-index.xml', 'extra/sitemap-statuspages-0.xml', 'extra/sitemap.xml']
+STATIC_PATHS = [
+    'images',
+    'extra/robots.txt',
+    'extra/favicon.ico',
+    'extra/sitemap-index.xml',
+    'extra/sitemap-statuspages-0.xml',
+    'extra/sitemap.xml',
+]
 
 EXTRA_PATH_METADATA = {
     'extra/robots.txt': {'path': 'robots.txt'},
@@ -41,8 +47,8 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = False
-DELETE_OUTPUT_DIRECTORY = True
+# DELETE_OUTPUT_DIRECTORY = True
 
 # Jinja2content plugin
 JINJA2CONTENT_TEMPLATES = ['content']
-LOAD_CONTENT_CACHE = False
+LOAD_CONTENT_CACHE = True
