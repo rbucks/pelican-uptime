7dce9feb90d2e78bb216519a39ac8c40c371a354
Taras Matsyk <x@tar7k.com>
2022-07-28 15:37:14 +0300

    Set uptime.com as a default URL root

--- publishconf.py
+++ publishconf.py
@@ -7,7 +7,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = os.getenv('SITEURL', 'http://localhost:8000')
+SITEURL = os.getenv('SITEURL', 'https://uptime.com')
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
